eb275e92508b435f93f71342001efe8a1d12d548
LUKE TRUITT <lot@duke.edu>
2019-06-15 19:29:34 -0400

    founders fund

--- SPEAR/src/app/app.component.ts
+++ SPEAR/src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent {
 
   constructor(
     private readonly router: Router,
-  ){
+  ) {
 
   }
 
--- SPEAR/src/app/app.module.ts
+++ SPEAR/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule  } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule, MatIconModule, MatMenuModule } from '@angular/material';
 import { RouterModule, ROUTES } from '@angular/router';
